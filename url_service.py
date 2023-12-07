18776dd83fe241da2ea02f8cf3e4f883a91ba323
Kevin <kevinwang4tw@yahoo.com>
2023-12-07 14:55:12 -0600

    Service logic implementation - reverted some port changes - fixed dynamo table - added unit test placeholder

--- url_service.py
+++ url_service.py
@@ -1,11 +1,15 @@
+from boto3.dynamodb.conditions import Key
+from fastapi import HTTPException
+
 from logger import get_logger
 import boto3
+import uuid
 
 table_name: str = "UrlMappingTable"
 dynamodb = boto3.resource(
     'dynamodb',
     region_name='local',
-    endpoint_url="http://dynamodb-local:8000",
+    endpoint_url="http://dynamodb-local:9000",
     aws_access_key_id='access',
     aws_secret_access_key='secret',
     aws_session_token='token'
@@ -19,14 +23,56 @@ class UrlService:
     def __init__(self):
         pass
 
+    def shorten_url(self, original_url: str) -> str:
+        logger.debug("shortening...")
+        try:
+            # url has already been shortened, returning existing short path
+            return self.get_id_from_db(original_url)
+        except Exception:
+            pass
+
+        new_url_id = str(uuid.uuid4())
+        self.save_url_to_db(new_url_id, original_url)
+
+        return new_url_id
+
+    def lengthen_url(self, short_url: str) -> str:
+        logger.debug("lengthening...")
+        return self.get_url_from_db(short_url)
+
     @staticmethod
-    def shorten_url(original_url: str) -> str:
-        logger.info("shortening...")
+    def get_id_from_db(url: str) -> str:
+        # query always return list
+        response = table.query(
+            IndexName='urlIdx',
+            KeyConditionExpression=Key('url').eq(url)
+        )
+        logger.debug(response)
 
-        return original_url
+        url_list = response.get("Items")
+        logger.debug(url_list)
+
+        if len(url_list) == 0:
+            raise HTTPException(status_code=404, detail="Url not found")
+
+        return url_list[0].get("urlId")
 
     @staticmethod
-    def lengthen_url(short_url: str) -> str:
-        logger.info("lengthening...")
+    def get_url_from_db(url_id: str) -> str:
+        # get always return one
+        response = table.get_item(Key={"urlId": url_id})
+        url = response.get("Item")
 
-        return short_url
+        if not url:
+            raise HTTPException(status_code=404, detail="Url not found")
+
+        return url.get("url")
+
+    @staticmethod
+    def save_url_to_db(url_id: str, url: str):
+        table.put_item(
+            Item={
+                "urlId": url_id,
+                "url": url,
+            }
+        )
